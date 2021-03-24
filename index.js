da122a942eb405b1792d71492f80897fee3aa5eb
TTatchell <tim.tatchell24@gmail.com>
2021-03-24 14:12:43 +1100

    upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//const divide = function(a,b) {
+//    return a / b};
+//This works for me but the 'test' doesn't supply numbers for the function
+
+
+let a = 2000;
+let b = 100;
+const divide = function() {
+    return a / b;
+}
+
+let square = (c) => c ** 2;
+
+let add = (a,b) => a+b;
